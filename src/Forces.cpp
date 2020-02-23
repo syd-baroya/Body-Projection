da5b6068b914f555a73b8eb19e67b31dbb2ad5d3
Emily O'Neal <emily.anna.oneal@gmail.com>
2020-02-23 09:55:02 -0800

    working on switching to conpute shader

--- src/Forces.cpp
+++ src/Forces.cpp
@@ -1,93 +1,8 @@
-#version 430 
-#extension GL_ARB_shader_storage_buffer_object : require
-
-//#define GRID_HEIGHT 20.0
-//#define GRID_WIDTH 20.0
+#include "Forces.h"
 
 
-layout(local_size_x = 1024, local_size_y = 1) in;	
-layout (binding = 0, offset = 0) uniform atomic_uint ac;
-
-//	for texture handling
-layout(rgba8, binding = 0) uniform image2D img;		//input/output image
-//		vec4 col=imageLoad(img_input, pixel_coords);
-//		imageStore(img_output, pixel_coords, col);
-
-
-
-//local group of shaders
-layout (std430, binding=0) volatile buffer shader_data
-{ 
-  ivec4 positions_list[1024];
-};
-uniform int sizeofbuffer;
-//uniform int firstTimeE;
-
-void main() 
-	{
-
-	
-	vec3 part[1024];
-	float x = 0.0;
-	int i = 0;
-	while (x <= 1.0) {
-		float y = 0.0;
-		while (y <= 1.0) {
-			//WHAT SHOULD Z BE?
-			part[i] = vec3(x, y, -1.618940);
-			i++;
-			y += 0.1;
-		}
-		x += 0.2;
-	}
-	for (int i = 0; i < 10; i++) {
-		//positions_list[i].x = part[i].x;
-		//positions_list[i].y = part[i].y;
-		//positions_list[i].z = part[i].z;
-		//printf("x: %f, y: %f \n", part[i].x,part[i].y);
-	}
-	uint index = gl_LocalInvocationID.x;
-	vec3 data = positions_list[index].xyz;
-	positions_list[index].y = int(data.x)*3;
-	atomicAdd(positions_list[0].y,1);
-	//could check if its the first time by seeing if z position equals zero
-	/*vector<vec3> particle_pos;
-	float x = 0.0;
-	while (x <= 1.0) {
-		float y = 0.0;
-		while (y <= 1.0) {
-			//WHAT SHOULD Z BE?
-			particle_pos.push_back(vec3(x, y, -1.618940));
-			y += 0.1;
-		}
-		x += 0.2;
-	}
-	//vec2 groups = createGroups();
-	//loop through posistion list
-	for (int i = 0; i < particle_pos.size(); i++) {
-		positions_list[i].x = particle_pos[i].x;
-		positions_list[i].y = particle_pos[i].y;
-		positions_list[i].z = particle_pos[i].z;
-	}*/
-		//print out positions on the grid we are looking at
-				
-		/*for (int i = 0; i < groups.size(); i++) {
-			printf("x: %f, y: %f \n", groups[i].x, groups[i].y);*/
-
-	
-
-
-	/*if (firstTimeE) {
-		particle_pos = createPart();
-		groups = createGroups();
-	}*/
-	//barrier();
-	//
-	}
-
-//forces functions
 //Function to create vector of particle positions
-/*vector<vec3> createPart()
+vector<vec3> createPart()
 {
 	//figure out exact pos to start particles at
 	vector<vec3> particle_pos;
@@ -115,9 +30,9 @@ vector<vec2> createGroups() {
 		while (y <= 5.0) {
 			groups.push_back(vec2(x, y));
 			nG += 1;
-			y += (2.0 / 20.0);
+			y += (2.0 / GRID_HEIGHT);
 		}
-		x += (2.0 /20.0);
+		x += (2.0 / GRID_WIDTH);
 	}
 	printf("There are %d groups\n", nG);
 	return groups;
@@ -170,4 +85,4 @@ bool within_box(vec2 a, vec2 b, float numG) {
 		return true;
 	}
 	return false;
-}*/
+}
